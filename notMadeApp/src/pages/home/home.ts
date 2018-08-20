3404137c2cbbe84395f933f7f9a4f3907c3ddf8d
Alexander Tepe <alexander.tepe@hotmail.de>
2018-08-20 22:18:32 +0200

    moved connection to bucket to script tag in home.html

--- notMadeApp/src/pages/home/home.ts
+++ notMadeApp/src/pages/home/home.ts
@@ -1,32 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import Cosmic from 'cosmicjs';
+
+// TODO change background color to dark
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  public gigs;
-
   constructor(public navCtrl: NavController) {
-      this.gigs = [];
 
-      // do this in loop + fix bucket call
-      this.reloadUpcomingGigs();
-  }
-
-  reloadUpcomingGigs() {
-    Cosmic.getObjectType({
-      bucket: {
-        slug: 'gigs-upcoming'
-      }
-    }, {
-      type_slug: 'upcoming gigs'
-    }, (err, res) => {
-      this.gigs = res.objecs.all;
-    });
   }
 
 }
